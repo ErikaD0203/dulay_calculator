67905f4df59d5a1e3618ea9a31ee9f05c8109702
ErikaD0203 <erikadulay12345678@gmail.com>
2021-01-19 10:59:57 +0800

    added changes

--- app/src/main/java/mcm/edu/ph/dulay_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/dulay_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package mcm.edu.ph.dulay_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnMinus,btnMultiplication,btnDivide,btnCLEAR:
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn0);
+
+
     }
 }
