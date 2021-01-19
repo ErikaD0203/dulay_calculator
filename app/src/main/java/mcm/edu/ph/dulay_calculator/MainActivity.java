17a9ee8d2f6a4d7b385527ad41c64b19bfe63f66
ErikaD0203 <erikadulay12345678@gmail.com>
2021-01-19 14:30:19 +0800

    added changes

--- app/src/main/java/mcm/edu/ph/dulay_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/dulay_calculator/MainActivity.java
@@ -12,11 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnMinus,btnMultiplication,btnDivide,btnCLEAR:
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnMinus,btnMultiplication,btnDivide,btnCLEAR;
 
         btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn0);
-
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
 
     }
 }
